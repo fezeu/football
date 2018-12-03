b78b53a1b56edd680d7d8a306eb21bb6bec5259b
fez2000 <eugenefezeu@gmail.com>
2018-12-03 19:24:42 +0100

    gestion des joueurs

---
+++ server/controllers/tournois.js
@@ -0,0 +1,27 @@
+var mongoose = require('mongoose'),
+Tournois = mongoose.model('Tournois');
+
+exports.intit = function(req,res){
+    if(typeof(req.session.auth) == 'undefined'){
+        return res.send({status:null,message:'AuhtError'}) 
+      }else{
+        autre = true
+        for(let elm of req.session.auth.tournois){
+            if(elm == req.body.id){
+                autre = false
+            }
+        }
+        if(autre){
+            return   res.send({status:false,message:'NotFound'})
+        }
+         Tournois.updateOne({'_id':req.body.id},{nom:req.body.nom,situation:req.body.situation,reglement:req.body.reglement,organisateurs:req.body.organisateurs,status:'incomplet'},function(err,resultat){
+            if(err){
+               return res.send({status:null, message:err})
+            }
+            return res.send({status:true})
+         })
+      }
+}
+exports.addJoueur = function(id_tournois,res){
+
+}
