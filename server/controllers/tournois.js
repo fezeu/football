825b7af86746261fca88b2a16b44360fb9fc0835
fez2000 <eugenefezeu@gmail.com>
2018-12-04 09:48:30 +0100

    ajout dossier init

--- server/controllers/tournois.js
+++ server/controllers/tournois.js
@@ -14,7 +14,7 @@ exports.intit = function(req,res){
         if(autre){
             return   res.send({status:false,message:'NotFound'})
         }
-         Tournois.updateOne({'_id':req.body.id},{nom:req.body.nom,situation:req.body.situation,reglement:req.body.reglement,organisateurs:req.body.organisateurs,status:'incomplet'},function(err,resultat){
+         Tournois.updateOne({'_id':req.body.id},{nom:req.body.nom,situation:req.body.situation,reglement:req.body.reglement,status:'incomplet'},function(err,resultat){
             if(err){
                return res.send({status:null, message:err})
             }
--- vue/src/app/competition/competition.module.ts
+++ vue/src/app/competition/competition.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { CompetitionRoutingModule } from './competition-routing.module';
 import { MainComponent } from './main/main.component';
+import { InitComponent } from './init/init.component';
 
 @NgModule({
   imports: [
     CommonModule,
     CompetitionRoutingModule
   ],
-  declarations: [MainComponent]
+  declarations: [MainComponent, InitComponent]
 })
 export class CompetitionModule { }
