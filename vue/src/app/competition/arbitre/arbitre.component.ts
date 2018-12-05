3ac46eb83342571181288d79ec92a38fb1e6334d
fez2000 <eugenefezeu@gmail.com>
2018-12-05 14:31:45 +0100

    'init'

--- vue/src/app/competition/arbitre/arbitre.component.ts
+++ vue/src/app/competition/arbitre/arbitre.component.ts
@@ -1,13 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input,Output, EventEmitter } from '@angular/core';
+import { CreateCompService } from '../create-comp.service';
 @Component({
   selector: 'app-arbitre',
   templateUrl: './arbitre.component.html',
   styleUrls: ['./arbitre.component.css']
 })
 export class ArbitreComponent implements OnInit {
-
-  constructor() { }
+  @Input('_id') _id;
+  @Output('initialiser')event:EventEmitter <any> = new EventEmitter();
+  constructor(
+    private comp : CreateCompService
+  ) { }
 
   ngOnInit() {
   }
